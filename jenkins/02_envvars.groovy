09967f7b68ad10f7f03907a090d03a649421243f
Kevin Kingsbury <kkingsbury@gmail.com>
2016-07-27 14:29:18 -0500

    Wrong 02_env file, fixed to one that works

--- jenkins/02_envvars.groovy
+++ jenkins/02_envvars.groovy
@@ -1,28 +1,19 @@
+import jenkins.*
+
 import jenkins.model.*
+
+import hudson.*
+
 import hudson.model.*
-import hudson.slaves.*
 
-String node_name = "master"
-String env_key = "AA"
-String env_value = "BB"
+import jenkins.model.Jenkins
+import hudson.slaves.EnvironmentVariablesNodeProperty
+import hudson.slaves.NodeProperty
 
 
-instance = Jenkins.getInstance()
-if (node_name == "master") {
-  node = instance
-} else {
-  instance.getNode(node_name)
-}
-props = node.nodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
-
-if(props.empty) {
-  def entry = new EnvironmentVariablesNodeProperty.Entry(env_key, env_value)
-  def evnp = new EnvironmentVariablesNodeProperty(entry)
-  node.nodeProperties.add(evnp)
-} else {
-  for (prop in props) {
-    prop.envVars.put(env_key, env_value)
-  }
+nodes = Jenkins.getInstance().getGlobalNodeProperties()
+def props = nodes.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+for (prop in props) {
+    prop.envVars.put("TEST_ENV", "WORKS")
 }
 
-instance.save()
