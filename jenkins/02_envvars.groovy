914f3441b48a7b8c4fe35e1f5eb757a0529c8bfb
Kevin Kingsbury <kkingsbury@gmail.com>
2016-07-28 08:44:34 -0500

    Snapshot, fixed 02, removed gitlab and nexus for now

--- jenkins/02_envvars.groovy
+++ jenkins/02_envvars.groovy
@@ -1,68 +1,42 @@
 import jenkins.*
-
 import jenkins.model.*
-
 import hudson.*
-
 import hudson.model.*
 
 import jenkins.model.Jenkins
 import hudson.slaves.EnvironmentVariablesNodeProperty
 import hudson.slaves.NodeProperty
 
+def env_var_map = [
+  'DO_WS_CLEANUP':'true',
+  'DOCKER_CERT_PATH':'/var/jenkins/keys',
+  'DOCKER_HOST':'tcp://10.121.145.60:443',
+  'DOCKER_TLS_VERIFY':'1',
+  'STARTING_PATH':'/var/jenkins_home',
+]
+
 
 nodes = Jenkins.getInstance().getGlobalNodeProperties()
-<<<<<<< Updated upstream
 def props = nodes.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
 for (prop in props) {
-    prop.envVars.put("TEST_ENV", "WORKS")
+  for (e in env_var_map)
+      {
+          prop.envVars.put("${e.key}", "${e.value}")
+      }
 }
-
-=======
-
-//def props = nodes.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
-
-def evnp = new EnvironmentVariablesNodeProperty();
-for (prop in evnp) {
-    prop.envVars.put("TEST_ENV", "WORKS")
-}
-
 
 /*
 def slaves = hudson.model.Hudson.instance.getNodes()
-for (slave in slaves) {
-    println "-----------------------"
-    println slave.name
-    println "-----------------------"
-
-    def props = slave.nodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
-    for (prop in props) {
-        prop.envVars.put("TEST_ENV", "WORKS")
-    }
-    //SAVE
+//nodes = Jenkins.getInstance().getGlobalNodeProperties()
+for (slave in slaves)
+{
+  //def props = nodes.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+  def props = slave.nodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+  for (prop in props) {
+      for (e in env_var_map)
+      {
+          prop.envVars.put("${e.key}", "${e.value}")
+      }
+  }
 }
-*/
-/*if ( nodes.size() != 1 ) {
 
- println("error: unexpected number of environment variable containers: "
-
-          + nodes.size()
-
-          + " expected: 1")
-
-} else {
-
- envVars= nodes.get(0).getEnvVars()
-
-  envVars.put("FOO", "foo")
-
-
-  Jenkins.getInstance().save()
-
-  println("okay")
-
-}
-*/
-
-
->>>>>>> Stashed changes
