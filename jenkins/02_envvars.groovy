dfd3f93b0433b43730c13cccfa80d51f803371bd
Kevin Kingsbury <kingsbury_kevin@bah.com>
2016-08-10 12:46:53 +0000

    Changed to work with Docker, added Env vars

--- jenkins/02_envvars.groovy
+++ jenkins/02_envvars.groovy
@@ -3,40 +3,39 @@ import jenkins.model.*
 import hudson.*
 import hudson.model.*
 
-import jenkins.model.Jenkins
-import hudson.slaves.EnvironmentVariablesNodeProperty
-import hudson.slaves.NodeProperty
+instance = Jenkins.getInstance()
+globalNodeProperties = instance.getGlobalNodeProperties()
+envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+
+newEnvVarsNodeProperty = null
+envVars = null
+
+if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+  newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+  globalNodeProperties.add(newEnvVarsNodeProperty)
+  envVars = newEnvVarsNodeProperty.getEnvVars()
+} else {
+  envVars = envVarsNodePropertyList.get(0).getEnvVars()
+
+}
 
 def env_var_map = [
   'DO_WS_CLEANUP':'true',
-  'DOCKER_CERT_PATH':'/var/jenkins/keys',
-  'DOCKER_HOST':'tcp://10.121.145.60:443',
+  'DOCKER_CERT_PATH':'/var/jenkins_home/keys',
+  'DOCKER_HOST':'tcp://10.0.0.1:443',
   'DOCKER_TLS_VERIFY':'1',
+  'DTR_HOST':'10.0.0.2',
+  'DTR_ORG':'orgname',
+  'DTR_PREFIX':'10.0.0.2/',
+  'NEXUS_USER':'admin',
   'STARTING_PATH':'/var/jenkins_home',
+  'UCP_HOST':'10.0.0.1'
 ]
 
-
-nodes = Jenkins.getInstance().getGlobalNodeProperties()
-def props = nodes.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
-for (prop in props) {
-  for (e in env_var_map)
+for (e in env_var_map)
       {
-          prop.envVars.put("${e.key}", "${e.value}")
+          envVars.put("${e.key}", "${e.value}")
+	  println("Adding: ${e.key} => ${e.value}")
       }
-}
 
-/*
-def slaves = hudson.model.Hudson.instance.getNodes()
-//nodes = Jenkins.getInstance().getGlobalNodeProperties()
-for (slave in slaves)
-{
-  //def props = nodes.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
-  def props = slave.nodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
-  for (prop in props) {
-      for (e in env_var_map)
-      {
-          prop.envVars.put("${e.key}", "${e.value}")
-      }
-  }
-}
-
+instance.save()
