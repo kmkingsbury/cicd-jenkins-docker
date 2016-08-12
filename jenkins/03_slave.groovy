ac37ab2c7ab0cf67172976f9867e44964ddf01b3
Kevin Kingsbury <kingsbury_kevin@bah.com>
2016-08-12 17:24:16 +0000

    Configure Jenkins SSH Slave

---
+++ jenkins/03_slave.groovy
@@ -0,0 +1,23 @@
+import jenkins.model.*
+import hudson.model.*
+import hudson.slaves.*
+import hudson.plugins.sshslaves.*
+import java.util.ArrayList;
+import hudson.slaves.EnvironmentVariablesNodeProperty.Entry;
+
+  List<Entry> env = new ArrayList<Entry>();
+  env.add(new Entry("key1","value1"))
+  env.add(new Entry("key2","value2"))
+  EnvironmentVariablesNodeProperty envPro = new EnvironmentVariablesNodeProperty(env);
+  Slave slave = new DumbSlave(
+                    "agent-node","Agent node description",
+                    "/home/jenkins",
+                    "1",
+                    Node.Mode.NORMAL,
+                    "agent-node-label",
+                    new SSHLauncher("agenNode",22,"user","password","","","","",""),
+                    new RetentionStrategy.Always(),
+                    new LinkedList())
+  slave.getNodeProperties().add(envPro)
+  Jenkins.instance.addNode(slave)
+
